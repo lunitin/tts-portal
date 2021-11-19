104737ee13ebc14ed1698b3410daf2fd8c9b29b9
Casey Dinsmore <lunitin@gmail.com>
2021-11-18 18:05:56 -0800

    Added static parameters to Flask and sample css file.

--- docroot/app/__init__.py
+++ docroot/app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
 from app import views
