38933a25d40ebbca1bd12030975cd20352e7de35
Casey Dinsmore <lunitin@gmail.com>
2022-01-24 17:29:58 -0800

    White space clean up

--- docroot/app/auth.py
+++ docroot/app/auth.py
@@ -3,6 +3,7 @@
 Flask routes that require authentication
 
 """
+
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user, logout_user
 from . import db
