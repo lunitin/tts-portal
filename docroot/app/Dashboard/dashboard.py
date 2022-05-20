a8d35a58fec943a6d2dac729765f12e5de641791
Casey Dinsmore <lunitin@gmail.com>
2022-05-19 21:54:03 -0700

    Whitespace and comment clean up

--- docroot/app/Dashboard/dashboard.py
+++ docroot/app/Dashboard/dashboard.py
@@ -1,3 +1,4 @@
+
 import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc, ctx
@@ -9,7 +10,7 @@ from .server_calls import get_coverages_by_user, get_signals_by_region, get_regi
 
 base_url = "/dash/app/"
 
-
+# Connect Dash up to Flask
 def init_dashboard(server):
     dash_app = dash.Dash(__name__,server=server,routes_pathname_prefix=base_url,external_stylesheets=['/css/bootstrap.css', '/css/dash.css'],)
     print("== init current user:", current_user)
@@ -342,11 +343,7 @@ def init(app):
 
             print("=== signals", signals, flush=True)
 
-            return signals #, True
-        # Fade in when a coverage is selected
-        # elif ctx.triggered_id == "coverage":
-        #     return [], True
-
+            return signals
         else:
             print("-- skipping region\n", flush=True)
             raise PreventUpdate
@@ -428,9 +425,7 @@ def init(app):
         # Fetch charts and labels
         if (region != None and signal != None):
 
-
             fig_td, lbl_td1, lbl_td2, lbl_td3 = get_totalDelayChart(signal, day, approach, direction)
-
 
             fig_ar, lbl_ar1, lbl_ar2 = get_arrivalPieChart(signal, day, approach, direction)
 
@@ -454,232 +449,3 @@ def init(app):
                 [], '','','',
                 [], []
              )
-
-
-
-
-# def init_callbacks(dash_app):
-#     return
-
-#content = html.Div([], id="page-content")
-#
-# # Defines the page content for any given light
-# def pageContent():
-#
-#     return [
-#         # Create Title And Dropdown
-#         dbc.Label("Broward " + str(light) + " Light", style={"text-align": 'center'}),
-#
-#         # Create Coverage Dropdown
-#         html.Div([
-#             html.H2("Coverage"),
-#             dcc.Dropdown(
-#                 id='coverage',
-#                 options=get_coverages_by_user()
-#             )
-#         ],
-#         style={"width": "10%"}),
-#
-#         # Create Region Dropdown
-#         html.Div(id='region-dropdown'),
-#
-#         # Create Signal Dropdown
-#         html.Div(id='signal-dropdown'),
-#
-#         # Create Day Dropdown
-#         html.Div([
-#             html.H2("Day"),
-#             dcc.Dropdown(
-#                 id='day',
-#                 options=[
-#                         {'label': 'Sunday', 'value': 1},
-#                         {'label': 'Monday', 'value': 2},
-#                         {'label': 'Tuesday', 'value': 3},
-#                         {'label': 'Wednesday', 'value': 4},
-#                         {'label': 'Thursday', 'value': 5},
-#                         {'label': 'Friday', 'value': 6},
-#                         {'label': 'Saturday', 'value': 7},
-#                 ],
-#                 value=1
-#             )
-#         ],
-#         style={"width": "10%"}),
-#
-#         # options=get_coverages(1) # replace with list to get user '1's coverages
-#
-#         # Create Dropdown for Approach
-#         html.Div([
-#             html.H2("Approach"),
-#             dcc.Dropdown(
-#                 id='approach',
-#                 options=[
-#                     {'label': 'Northbound', 'value': 'Northbound'},
-#                     {'label': 'Eastbound', 'value': 'Eastbound'},
-#                     {'label': 'Southbound', 'value': 'Southbound'},
-#                     {'label': 'Westbound', 'value': 'Westbound'},
-#                     {'label': 'ALL', 'value': 'ALL'},
-#                 ],
-#                 value='ALL'
-#             )
-#         ],
-#         style={"width": "10%"}),
-#
-#         # Create dropdwon for travel direction
-#         html.H2("Travel Direction"),
-#         html.Div([
-#             dcc.Dropdown(
-#                 id='tdirection',
-#                 options=[
-#                     {'label': 'Straight', 'value': 'Straight'},
-#                     {'label': 'Left', 'value': 'Left'},
-#                     {'label': 'Right', 'value': 'Right'},
-#                     {'label': 'ALL', 'value': 'ALL'},
-#                 ],
-#                 value='ALL'
-#             )
-#         ],
-#         style={'width': '10%'}),
-#
-#         # Create Total Delay Chart
-#         html.Div([
-#             dcc.Graph(id='total-delay-chart'),
-#             html.H5(id="avg-delay", style={'padding-left': '150px'}),
-#             html.H5(id="total-delay", style={'padding-left': '150px'}),
-#             html.H5(id="total-delay-crossings", style={'padding-left': '150px'}),
-#         ], style={"display": "inline-block", "width": "30%", "vertical-align": "top"}),
-#
-#         # Create Arrivals Chart
-#         html.Div([
-#             dcc.Graph(id='arrival-chart'),
-#             html.H5(id='green-Arrival-Rate', style={'padding-left': '150px'}),
-#             html.H5(id='arrival-Crossings', style={'padding-left': '150px'})
-#         ], style={"width": "30%", "display": "inline-block"}),
-#
-#         # Create Split Failure Chart
-#         html.Div([
-#             dcc.Graph(id='split-fail'),
-#             html.H5(id='Split-Rate', style={'padding-left': '150px'}),
-#             html.H5(id='Total-Split-Fail', style={'padding-left': '150px'}),
-#             html.H5(id='Split-Crossing', style={'padding-left': '150px'})
-#         ], style={"display": "inline-block", "width": "30%", "vertical-align": "top"}),
-#
-#         # movement and approachDirection bar chart
-#         html.Div([
-#             dcc.Graph(id='movement'),
-#         ]),
-#
-#         html.Div([
-#             dcc.Graph(id='delayScatter'),
-#         ])
-#     ]
-#
-#
-# # Makes a pie chart for any given light
-# def arrivalPieChart(light, day, approach, tdirection):
-#     arrivalRates, greenArrivalRate, arrivalCrossings = get_arrivalPieChart(light, day, approach, tdirection)
-#     greenArrivalRateStr = "Green Arrival Rate: {}%".format(greenArrivalRate)
-#     arrivalCrossingsStr = "Arrival Crossings: {}".format(arrivalCrossings)
-#     return arrivalRates, greenArrivalRateStr, arrivalCrossingsStr
-#
-# # Makes a split failure pie chart for any given light
-# def splitPieChart(light, day, approach, tdirection):
-#     splitFailure, splitCrossing, totalSplitFailure, SplitRate = get_splitPieChart(light, day, approach, tdirection)
-#     splitCrossingStr = "Split Failure Crossings: {}".format(splitCrossing)
-#     totalSplitFailureStr = "Total Split Failures: {}".format(totalSplitFailure)
-#     SplitRateStr = "Split Failure Rate: {}%".format(SplitRate)
-#
-#     return splitFailure, splitCrossingStr, totalSplitFailureStr, SplitRateStr
-#
-# def totalDelayChart(light, day, approach, tdirection):
-#     fig, delayCrossingsStr, avgDelayStr, totalDelayStr = get_totalDelayChart(light, day, approach, tdirection)
-#     return fig, delayCrossingsStr, avgDelayStr, totalDelayStr
-#
-# #subplots scatterPlot? with histogram
-# #dropdown with different variable types, delay, splitfailure, arrival
-# #hour and delay in a scatter, by movement
-# #stacked bar chart with different movements in each bar for approach direction
-# #hours, minutes, seconds columns? for df
-# def movementBarChart(light, day, approach, tdirection):
-#     moveM = get_movementBarChart(light, day, approach, tdirection)
-#     return moveM
-#
-# def scatterPlot(light, day, approach, tdirection):
-#     peakScatter = get_peakScatterPlot(light, day, approach, tdirection)
-#     return peakScatter
-#
-# def init_callbacks(dash_app):
-#     # Callback for 3084 charts
-#     @dash_app.callback(
-#         [Output(component_id='arrival-chart', component_property='figure'),
-#         Output(component_id='split-fail', component_property='figure'),
-#         Output(component_id='green-Arrival-Rate', component_property='children'),
-#         Output(component_id='arrival-Crossings', component_property='children'),
-#         Output(component_id='Split-Crossing', component_property='children'),
-#         Output(component_id='Total-Split-Fail', component_property='children'),
-#         Output(component_id='Split-Rate', component_property='children'),
-#         Output(component_id='total-delay-chart', component_property='figure'),
-#         Output(component_id='avg-delay', component_property='children'),
-#         Output(component_id='total-delay', component_property='children'),
-#         Output(component_id='total-delay-crossings', component_property='children'),
-#         Output(component_id='delayScatter', component_property='figure'),
-#         Output(component_id='movement', component_property='figure')],
-#         [Input(component_id='day', component_property='value'),
-#         Input(component_id='approach', component_property='value'),
-#         Input(component_id='tdirection', component_property='value'),
-#         Input(component_id='coverage', component_property='value'),
-#         Input(component_id='region-dropdown', component_property='value'),
-#         Input(component_id='signal-dropdown', component_property='value')]
-#     )
-#     def generate_chart(day, approach, tdirection, coverage, region, signal):
-#
-#         arrivalRates = arrivalPieChart('3084', day, approach, tdirection)
-#         splitFailure = splitPieChart('3084', day, approach, tdirection)
-#         totalDelay = totalDelayChart('3084', day, approach, tdirection)
-#         peakScatter = scatterPlot('3084', day, approach, tdirection)
-#         movement = movementBarChart('3084', day, approach, tdirection)
-#         return arrivalRates[0], splitFailure[0], arrivalRates[1], arrivalRates[2], splitFailure[1], splitFailure[2], splitFailure[3], totalDelay[0], totalDelay[2], totalDelay[3], totalDelay[1], peakScatter, movement
-#
-#     # This callback uses the above function to return what belongs on the page
-#     @dash_app.callback(
-#         Output("page-content", "children"),
-#         [Input("url", "pathname")]
-#     )
-#     def page_content(pathname):
-#
-#         # Ensure we're an authenticated user before rendering any content
-#         if not current_user or not current_user.is_authenticated:
-#             return html.Div(strings.ERROR_PAGE_PERMISSION_DENIED)
-#
-#         # Check here to ensure that we have access to the requested coverage area
-#
-#         # Different page content depending on which page we are pn
-#         #if pathname == "/dashboard/":
-#         return pageContent()
-#
-#     # Callbacks for region
-#     @dash_app.callback(
-#         Output('region-dropdown', 'children'),
-#         [Input('coverage', 'value')]
-#     )
-#     def render_region_dropdown(coverage):
-#         return html.Div([
-#             html.H2("Region"),
-#             dcc.Dropdown(
-#                 id='region',
-#                 options=get_regions_by_coverage(coverage)
-#             ),
-#         ],style={"width": "10%"})
-#
-#     # Callbacks for signal
-#     @dash_app.callback(
-#         Output('signal-dropdown', 'children'),
-#         [Input('region', 'value')]
-#     )
-#     def render_signal_dropdown(region):
-#         return html.Div([
-#             html.H2("Signal"),
-#             dcc.Dropdown(
-#                 id='signal',
-#                 options=get_signals_by_region(region)
-#             ),
-#         ],style={"width": "10%"})
